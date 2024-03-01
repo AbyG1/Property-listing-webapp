cf0337b020dd8e9fae5a39e09259fe51d3760b7e
AbyG1 <abygeorge133@gmail.com>
2024-03-01 10:09:45 +0530

    Add redux toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,13 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css'
 
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+ 
+
 )
