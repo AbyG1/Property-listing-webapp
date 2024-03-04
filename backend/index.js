e370e18b9e18878bff0026f133bb5c51f8cb40ff
AbyG1 <abygeorge133@gmail.com>
2024-03-04 20:21:21 +0530

    Add update user fuctionality

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 
 
@@ -24,6 +25,8 @@ const app = express();
 
 app.use(express.json())
 
+app.use(cookieParser())
+
 app.listen(3000,() => {
     console.log('Server is running on port 3000!');
     }
@@ -34,6 +37,7 @@ app.listen(3000,() => {
 app.use("/api/user",userRouter)
 
 app.use("/api/auth",authRouter)
+
 
 
 app.use((err, req, res, next) => {
@@ -47,3 +51,4 @@ app.use((err, req, res, next) => {
     })
 
 })
+
--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -1,53 +1,166 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
 import { app } from '../firebase';
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  
 
-
-function Profile() {
-  const { currentUser} = useSelector((state) => state.user)
-  const fileRef = useRef(null)
-  const [file, setFile] = useState(undefined)
+} from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+export default function Profile() {
+  const fileRef = useRef(null);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
-  const [fileUploadError, setFileUploadError] = useState(false)
-  const [formData, setFormData] = useState({})
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
+  const dispatch = useDispatch();
+
+  
 
   useEffect(() => {
-    if(file){
-      handleFileUpload(file)
+    if (file) {
+      handleFileUpload(file);
     }
-  },[file])
+  }, [file]);
 
   const handleFileUpload = (file) => {
-      const storage = getStorage(app)
-      const fileName = new Date().getTime() + file.name
-      const storageRef = ref(storage, fileName) 
-      const uploadTask = uploadBytesResumable(storageRef, file)
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-      uploadTask.on('state_changed',
+    uploadTask.on(
+      'state_changed',
       (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setFilePerc(Math.round(progress))
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
       },
       (error) => {
-        setFileUploadError(true)
+        setFileUploadError(true);
       },
       () => {
-        getDownloadURL(uploadTask.snapshot.ref).then
-        ((downloadURL) => {
-            setFormData({...formData, avatar:downloadURL})
-        })
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
       }
-      )
-  }
+    );
+  };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
+  // const handleDeleteUser = async () => {
+  //   try {
+  //     dispatch(deleteUserStart());
+  //     const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+  //       method: 'DELETE',
+  //     });
+  //     const data = await res.json();
+  //     if (data.success === false) {
+  //       dispatch(deleteUserFailure(data.message));
+  //       return;
+  //     }
+  //     dispatch(deleteUserSuccess(data));
+  //   } catch (error) {
+  //     dispatch(deleteUserFailure(error.message));
+  //   }
+  // };
+
+  // const handleSignOut = async () => {
+  //   try {
+  //     dispatch(signOutUserStart());
+  //     const res = await fetch('/api/auth/signout');
+  //     const data = await res.json();
+  //     if (data.success === false) {
+  //       dispatch(deleteUserFailure(data.message));
+  //       return;
+  //     }
+  //     dispatch(deleteUserSuccess(data));
+  //   } catch (error) {
+  //     dispatch(deleteUserFailure(data.message));
+  //   }
+  // };
+
+  // const handleShowListings = async () => {
+  //   try {
+  //     setShowListingsError(false);
+  //     const res = await fetch(`/api/user/listings/${currentUser._id}`);
+  //     const data = await res.json();
+  //     if (data.success === false) {
+  //       setShowListingsError(true);
+  //       return;
+  //     }
+
+  //     setUserListings(data);
+  //   } catch (error) {
+  //     setShowListingsError(true);
+  //   }
+  // };
+
+  // const handleListingDelete = async (listingId) => {
+  //   try {
+  //     const res = await fetch(`/api/listing/delete/${listingId}`, {
+  //       method: 'DELETE',
+  //     });
+  //     const data = await res.json();
+  //     if (data.success === false) {
+  //       console.log(data.message);
+  //       return;
+  //     }
+
+  //     setUserListings((prev) =>
+  //       prev.filter((listing) => listing._id !== listingId)
+  //     );
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // };
   return (
     <div className='container'>
       <div className="row justify-content-center">
         <div className="col-md-6">
           <h1 className='text-center mt-4'>Profile</h1>
-          <form className='d-flex flex-column pb-2'>
+          <form onSubmit={handleSubmit} className='d-flex flex-column pb-2'>
             <input onChange={(e) => setFile(e.target.files[0])}   type="file" ref={fileRef} hidden accept='image/*' />
             <div className='text-center m-3'>
               <img onClick={() => fileRef.current.click() }src={formData.avatar || currentUser.avatar} alt="profile" className='rounded-pill w-25' />
@@ -66,10 +179,14 @@ function Profile() {
         </p>
             </div>
               
-              <TextField id="username" label="username" variant="outlined" className='mb-3' />
-              <TextField id="email" label="email" variant="outlined" className='mb-3' />
-              <TextField id="password" label="password" variant="outlined" className='mb-3' />
-              <button className='p-3 btn btn-success'>Update</button>
+              <TextField onChange={handleChange}defaultValue={currentUser.username} id="username" label="username" variant="outlined" className='mb-3' />
+              <TextField onChange={handleChange}defaultValue={currentUser.email} id="email" label="email" variant="outlined" className='mb-3' />
+              <TextField type='password' id="password" label="password" variant="outlined" className='mb-3' />
+              <button disabled={loading} className='p-3 btn btn-success'>
+                  {
+                    loading ? 'Loading...' : "Update"
+                  }
+              </button>
 
 
           </form>
@@ -78,15 +195,15 @@ function Profile() {
             <button className='btn btn-danger w-100 p-3'>Sign Out</button>
             <p className='text-danger m-2'>Delete User</p>
           </div>
-
+          <p className='text-danger'>{error ? error : " "}</p>
+          <p className='text-success'>{updateSuccess ? 'User is updated successfully' : " "}</p>
         </div>
       </div>
     </div>
   )
 }
 
-export default Profile
 
 
 
-{/* */}
+
