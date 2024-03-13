32853d61e3747d032a03bc8afcc79e62ecfff79d
AbyG1 <abygeorge133@gmail.com>
2024-03-13 20:42:12 +0530

    Update createlisting page

--- frontend/src/Pages/CreateListing.jsx
+++ frontend/src/Pages/CreateListing.jsx
@@ -40,30 +40,30 @@ function CreateListing() {
                                 <input type="text" placeholder="Address" className='form-control mb-2'/>
                                 
                                 <div className=" d-flex w-100 my-2">
-                                      <input type="checkbox" id='sell' className="ms-2 me-1"/>
+                                      <input type="checkbox" id='sell' className="ms-md-2 me-md-1"/>
                                       <label htmlFor="sell">Sell</label>
-                                      <input type="checkbox" id='rent' className="ms-2 me-1 "/>
+                                      <input type="checkbox" id='rent' className="ms-md-2 me-md-1 "/>
                                       <label htmlFor="rent">Rent/Lease</label>
 
 
                                 </div>
 
                                 <div className='d-flex w-100'>
-                                        <input type="checkbox" className="ms-2 me-1 " id="offer" />
+                                        <input type="checkbox" className="ms-md-2 me-md-1 " id="offer" />
                                         <label htmlFor="offer">Offer</label>
                                 </div>
 
                                     <div className='d-flex w-100 my-2 '>
                                           <div d-flex align-items-center> 
-                                            <input type="checkbox" className="ms-2 me-1 " name='' value="" id='house' />
+                                            <input type="checkbox" className="ms-md-2 me-md-1 " name='' value="" id='house' />
                                             <label htmlFor="house">House</label>
                                           </div>
                                           <div d-flex align-items-center>
-                                                <input type="checkbox" className="ms-2 me-1 " name='' value="" id='apartment' />
+                                                <input type="checkbox" className="ms-md-2 me-md-1 " name='' value="" id='apartment' />
                                               <label htmlFor="apartment">Apartment</label>
                                           </div>
                                           <div d-flex align-items-center>
-                                              <input type="checkbox" className="ms-2 me-1 " name='' value="" id='rland' />
+                                              <input type="checkbox" className="ms-md-2 me-md-1 " name='' value="" id='rland' />
                                               <label htmlFor="rland">Residential Land</label>
                                           </div>
                                         
@@ -72,9 +72,9 @@ function CreateListing() {
                                     </div>
 
                                     <div className='d-flex w-100 my-2'>
-                                        <input type="checkbox" className="ms-2 me-1 " id='parking' />
+                                        <input type="checkbox" className="ms-md-2 me-md-1 " id='parking' />
                                         <label htmlFor="parking">Parking spot</label>
-                                        <input type="checkbox" className="ms-2 me-1 " id="furnished" />
+                                        <input type="checkbox" className="ms-md-2 me-md-1 " id="furnished" />
                                         <label htmlFor="furnished">Furnished</label>
                                       
         
@@ -82,17 +82,17 @@ function CreateListing() {
 
                                     <div className='d-flex w-100  my-2'>
                                           <div className=' d-flex align-items-center mb-2'>
-                                            <input type="number" className='form-control w-50 me-2' id="bedrooms" min="1" max="10" required/>
+                                            <input type="number" className='form-control w-50 me-md-2' id="bedrooms" min="1" max="10" required/>
                                             <label htmlFor="bedrooms">Beds</label>
                                           </div >
                                           <div className=' d-flex align-items-center mb-2'>
-                                            <input type="number" className='form-control w-50 me-2' id="bathrooms" min="1" max="10" required/>
+                                            <input type="number" className='form-control w-50 me-md-2' id="bathrooms" min="1" max="10" required/>
                                             <label htmlFor="bathrooms">Baths</label>
                                           </div>
                                     </div>
                                     <div className='d-flex w-100'>
                                             <div className=' d-flex  w-100 '>
-                                                <input type="number" className='form-control w-50 me-2' id="regularPrice" min="1" max="10" required/>
+                                                <input type="number" className='form-control w-50 me-md-2' id="regularPrice" min="1" max="10" required/>
                                                 <div>
                                                   <label htmlFor="regularPrice">Regular Price</label>
                                                 <small><label htmlFor="regularPrice" className='text-muted'>(Rs/month)</label></small>
@@ -101,7 +101,7 @@ function CreateListing() {
                                                 
                                             </div>
                                             <div className=' d-flex  w-100 '>
-                                                <input type="number" className='form-control w-50 me-2 ' id="discountPrice" min="1" max="10" required/>
+                                                <input type="number" className='form-control w-50 me-md-2 ' id="discountPrice" min="1" max="10" required/>
                                                 <div>
                                                   <label htmlFor="discountPrice">Discounted Price</label>
                                                   <small> <label htmlFor="discountPrice" className='text-muted'>(Rs/month)</label></small>
@@ -114,22 +114,22 @@ function CreateListing() {
                               
                               <div className='d-flex w-100 my-2 flex-column'>
 
-                                  <div className='d-flex align-items-center me-2 mb-1 '>
+                                  <div className='d-flex align-items-center me-md-2 mb-1 '>
                                       <label htmlFor="area" className='w-25'>PloatArea</label>
                                       <input type="number" className='form-control w-50' id="area"  min="1" required />
                                       <label htmlFor="area">sqft</label>
                                   </div>
-                                  <div className='d-flex align-items-center me-2 mb-1'>
+                                  <div className='d-flex align-items-center me-md-2 mb-1'>
                                       <label htmlFor="length" className='w-25'>Length</label>
                                       <input type="number" className='form-control w-50' id="length"  min="1" required/>
                                       <label htmlFor="length">ft</label>
                                   </div>
-                                  <div className='d-flex align-items-center me-2 mb-1' >
+                                  <div className='d-flex align-items-center me-md-2 mb-1' >
                                     <label htmlFor="width" className='w-25'>Width</label>
                                     <input type="number" className='form-control w-50' id="width"  min="1" required />
                                     <label htmlFor="width" > ft</label>
                                   </div>
-                                  <div className='d-flex align-items-center me-2 mb-1' >
+                                  <div className='d-flex align-items-center me-md-2 mb-1' >
                                     <label htmlFor="roadwidth" className='w-25'>Width of facing road</label>
                                     <input type="number" className='form-control w-50' id="roadwidth"  min="1" required />
                                     <label htmlFor="roadwidth" > ft</label>
@@ -137,11 +137,11 @@ function CreateListing() {
 
                               </div>
 
-                               <div className='d-flex flex-column'>
+                              <div className='d-flex flex-column w-100'>
                                   <p><span className='fw-bold'>Images:</span>The first image will be the cover (max 6)</p>
-                                  <div className='d-flex mb-2 gap-5'>
-                                    <input  className="p-3 border "type="file" id="images" accept="image/*" multiple />
-                                    <button className='btn btn-bg-success'>Upload</button>
+                                  <div className='d-flex mb-2 gap-md-5'>
+                                    <input  className="border "type="file" id="images" accept="image/*" multiple />
+                                    <button className='btn btn-outline-success'>Upload</button>
                                   </div>
                                 
                                 
