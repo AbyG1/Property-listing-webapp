d20c2d3eb3ba0737c248b41970bfe9d531bafe01
AbyG1 <abygeorge133@gmail.com>
2024-04-09 19:07:26 +0530

    Add contact landlord functionality

--- frontend/src/Pages/Listing.jsx
+++ frontend/src/Pages/Listing.jsx
@@ -1,25 +1,25 @@
 import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
-
 import { useSelector } from 'react-redux';
-
-
-
-// import Contact from '../components/Contact';
+import React from 'react';
+import Contact from '../Components/Contact';
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
-  // SwiperCore.use([Navigation]);
+
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [copied, setCopied] = useState(false);
   const [contact, setContact] = useState(false);
   const params = useParams();
   const { currentUser } = useSelector((state) => state.user);
+  const [centredModal, setCentredModal] = useState(false);
+
+  const toggleOpen = () => setCentredModal(!centredModal);
+
+
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -43,6 +43,8 @@ export default function Listing() {
     fetchListing();
   }, [params.listingId]);
   console.log(listing)
+
+  
 
   return (
 
@@ -92,7 +94,8 @@ export default function Listing() {
       </>
     )}
   </p>
-  <button className="btn btn-success btn-lg">Contact Seller</button>
+  
+  
 </div>
 
               </div>
@@ -183,11 +186,21 @@ export default function Listing() {
                     <h5 className='border-bottom'>Description</h5>
                     <p>{listing.description}</p>
                   </div>
-                  
+
+                  {currentUser  && listing.userRef !== currentUser._id && !contact &&(
+  
+                   <button onClick={()=>setContact(true)} className="btn btn-success w-100">Contact Landlord</button>
+                )}
+                  {contact && <Contact listing={listing}/>}
+
+
+
                 </div>
               </div>
 
 
+
+                                
 
 
 
@@ -199,76 +212,17 @@ export default function Listing() {
 
 
 
+
+
+
       )}
 
     </div>
-    // <main>
-    //   
-    //       <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
-    //         <FaShare
-    //           className='text-slate-500'
-    //           onClick={() => {
-    //             navigator.clipboard.writeText(window.location.href);
-    //             setCopied(true);
-    //             setTimeout(() => {
-    //               setCopied(false);
-    //             }, 2000);
-    //           }}
-    //         />
-    //       </div>
-    //       {copied && (
-    //         <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
-    //           Link copied!
-    //         </p>
-    //       )}
-    //       <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
-    //        
-    //         
-    //         <div className='flex gap-4'>
-    //           <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-    //             {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
-    //           </p>
-    //           {listing.offer && (
-    //             <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-    //               ${+listing.regularPrice - +listing.discountPrice} OFF
-    //             </p>
-    //           )}
-    //         </div>
-    //         
-    //         <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
-    //           <li className='flex items-center gap-1 whitespace-nowrap '>
-    //             <FaBed className='text-lg' />
-    //             {listing.bedrooms > 1
-    //               ? `${listing.bedrooms} beds `
-    //               : `${listing.bedrooms} bed `}
-    //           </li>
-    //           <li className='flex items-center gap-1 whitespace-nowrap '>
-    //             <FaBath className='text-lg' />
-    //             {listing.bathrooms > 1
-    //               ? `${listing.bathrooms} baths `
-    //               : `${listing.bathrooms} bath `}
-    //           </li>
-    //           <li className='flex items-center gap-1 whitespace-nowrap '>
-    //             <FaParking className='text-lg' />
-    //             {listing.parking ? 'Parking spot' : 'No Parking'}
-    //           </li>
-    //           <li className='flex items-center gap-1 whitespace-nowrap '>
-    //             <FaChair className='text-lg' />
-    //             {listing.furnished ? 'Furnished' : 'Unfurnished'}
-    //           </li>
-    //         </ul>
-    //         {currentUser && listing.userRef !== currentUser._id && !contact && (
-    //           <button
-    //             onClick={() => setContact(true)}
-    //             className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
-    //           >
-    //             Contact landlord
-    //           </button>
-    //         )}
-    //         {contact && <Contact listing={listing} />}
-    //       </div>
-    //     </div>
-    //   )}
-    // </main>
+
   );
 }
+
+
+
+
+
