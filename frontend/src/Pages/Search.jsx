1fdc7cf01aabfdcc0fc98cad3a3407558134bcd6
AbyG1 <abygeorge133@gmail.com>
2024-04-18 19:00:56 +0530

    Add show more functionality

--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -19,8 +19,8 @@ function Search() {
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-//   const [showMore, setShowMore] = useState(false);
-    console.log(listings)
+    const [showMore, setShowMore] = useState(false);
+   
 
     useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -57,14 +57,14 @@ function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
-      // setShowMore(false);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
       if (data.length > 8) {
-        // setShowMore(true);
+        setShowMore(true);
       } else {
-        // setShowMore(false);
+        setShowMore(false);
       }
       setListings(data);
       setLoading(false);
@@ -133,6 +133,21 @@ function Search() {
     urlParams.set('order', sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
   };
 
   
@@ -303,6 +318,22 @@ function Search() {
                                   <ListingItem key={listing._id} listing={listing}/>
                                 ))
                                 }
+
+
+                                <div className='p-4'>
+                                   {showMore && (
+                                            <button
+                                              onClick={onShowMoreClick}
+                                              className='btn btn-primary'
+                                            >
+                                              Show more
+                                            </button>
+                                )}
+                                </div>
+                               
+
+
+
                           </div>                          
                           
                        
@@ -333,54 +364,5 @@ export default Search
 
 
 
-//   const onShowMoreClick = async () => {
-//     const numberOfListings = listings.length;
-//     const startIndex = numberOfListings;
-//     const urlParams = new URLSearchParams(location.search);
-//     urlParams.set('startIndex', startIndex);
-//     const searchQuery = urlParams.toString();
-//     const res = await fetch(`/api/listing/get?${searchQuery}`);
-//     const data = await res.json();
-//     if (data.length < 9) {
-//       setShowMore(false);
-//     }
-//     setListings([...listings, ...data]);
-//   };
-//   return (
-//     <div className='flex flex-col md:flex-row'>
-//       <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
-//        
-//       </div>
-//       <div className='flex-1'>
-//         <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
-//           Listing results:
-//         </h1>
-//         <div className='p-7 flex flex-wrap gap-4'>
-//           {!loading && listings.length === 0 && (
-//             <p className='text-xl text-slate-700'>No listing found!</p>
-//           )}
-//           {loading && (
-//             <p className='text-xl text-slate-700 text-center w-full'>
-//               Loading...
-//             </p>
-//           )}
 
-//           {!loading &&
-//             listings &&
-//             listings.map((listing) => (
-//               <ListingItem key={listing._id} listing={listing} />
-//             ))}
 
-//           {showMore && (
-//             <button
-//               onClick={onShowMoreClick}
-//               className='text-green-700 hover:underline p-7 text-center w-full'
-//             >
-//               Show more
-//             </button>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
